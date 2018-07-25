91b6b6d244abee0ad4c964f4219bd43797e59e68
Thiago Santos <thiagogyn1@hotmail.com>
2018-07-25 15:46:18 -0500

    update

---
+++ LR_cnn.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 24 12:16:07 2018
+
+@author: thiago
+"""
+
+# Project Designed for Letter Recognition(digital or handwritten)
+
+# Importing the Keras libraries and packages
+from keras.models import Sequential
+from keras.layers import Convolution2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
+import numpy as np
+
+from skimage.io import imread
+from skimage.transform import resize
+from keras.preprocessing.image import ImageDataGenerator
+
+# Initialising the CNN
+classifier = Sequential()
+
+# Step 1 - Convolution
+classifier.add(Convolution2D(32, 2, 2, input_shape = (32, 32, 3), activation = 'relu'))
+
+# Step 2 - Pooling
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
+# Adding a second convolutional layer & pooling
+classifier.add(Convolution2D(64, 2, 2, activation = 'relu'))
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
+# Adding a third convolutional layer & pooling
+classifier.add(Convolution2D(128, 2, 2, activation = 'relu'))
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
+# Step 3 - Flattening
+classifier.add(Flatten())
+
+# Step 4 - Full connection
+# Hidden layers
+classifier.add(Dense(output_dim = 256, activation = 'relu'))
+classifier.add(Dense(output_dim = 256, activation = 'relu'))
+
+#output layer
+classifier.add(Dense(output_dim = 26, activation = 'softmax'))
+
+# Compiling the CNN
+classifier.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
+
+
+# Part 2 - Fitting the CNN to the images
+
+train_datagen = ImageDataGenerator( rescale=1./255,
+                                    shear_range=0.2,
+                                    zoom_range=0.2,
+                                    horizontal_flip=True)
+
+
+test_datagen = ImageDataGenerator(rescale = 1./255)
+
+
+training_set = train_datagen.flow_from_directory('Dataset/training_set',
+                                                 target_size = (32, 32),
+                                                 batch_size = 32,
+                                                 class_mode = 'categorical')
+
+test_set = test_datagen.flow_from_directory('Dataset/test_set',
+                                            target_size = (32, 32),
+                                            batch_size = 32,
+                                            class_mode = 'categorical')
+
+classifier.fit_generator(training_set,
+                         steps_per_epoch = (16384/32),
+                         epochs = 50,
+                         validation_data = test_set,
+                         validation_steps = (6300/32))
+
+
+
